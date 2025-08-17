0dda4288e9e98429b6e3edab43b8c3e68b9ef307
POPASMALINOIS <j.m.p.garrido87@outlook.es>
2025-08-18 01:33:39 +0200

    Update Operacion.cs

--- src/OperativaLogistica/Models/Operacion.cs
+++ src/OperativaLogistica/Models/Operacion.cs
@@ -10,16 +10,23 @@ namespace OperativaLogistica.Models
         public string Transportista { get; set; } = "";
         public string Matricula { get; set; } = "";
         public string Muelle { get; set; } = "";
-        public string Estado { get; set; } = "";                 // Combobox
+
+        // Desplegable
+        public string Estado { get; set; } = "";
+
         public string Destino { get; set; } = "";
         public string Llegada { get; set; } = "";
         public string? LlegadaReal { get; set; }
         public string? SalidaReal { get; set; }
         public string SalidaTope { get; set; } = "";
         public string Observaciones { get; set; } = "";
-        public string Incidencias { get; set; } = "";             // Combobox
-        public string Precinto { get; set; } = "";                // NUEVO
-        public bool Lex { get; set; } = false;                    // NUEVO
+
+        // Desplegable
+        public string Incidencias { get; set; } = "";
+
+        // Nuevos
+        public string Precinto { get; set; } = "";
+        public bool Lex { get; set; } = false;
 
         [Browsable(false)] public DateOnly Fecha { get; set; } = DateOnly.FromDateTime(DateTime.Now);
     }
